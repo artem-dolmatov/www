ef855edb5c6bbf5ccbc1d978826810cb829b1650
Artem <artem.v.dolmatov@gmail.com>
2017-06-09 14:57:38 +0500

    v-3.0

---
+++ tobolsk-school.ru/routes/index.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//Any requests to this controller must pass through this 'use' function
+//Copy and pasted from method-override
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+}))
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Автошколы Екатеринбурга' });
+// });
+
+//build the REST operations at the base for blobs
+//this will be accessible from http://127.0.0.1:3000/blobs if the default route for / is left unchanged
+router.route('/')
+    //GET all blobs
+    .get(function(req, res, next) {
+        //retrieve all blobs from Monogo
+        mongoose.model('School').find({}, function (err, schools) {
+              if (err) {
+                  return console.error(err);
+              } else {
+                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                  res.format({
+                      //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
+                    html: function(){
+                        res.render('index', {
+                              title: 'Автошколы Екатеринбурга',
+                              "schools" : schools
+                          });
+                    },
+                    //JSON response will show all blobs in JSON format
+                    json: function(){
+                        res.json(schools);
+                    }
+                });
+              }
+        });
+    })
+
+router.post('/', function(req, res){
+  var api_key = 'key-430a017b2d04af9af2d9cfd80e7a2c08';
+  var domain = 'sandboxc1a42f17212546f1b27b27d149b04546.mailgun.org';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+  var data = {
+    from: 'as196.ru <postmaster@sandboxc1a42f17212546f1b27b27d149b04546.mailgun.org>',
+    to: 'tmn-as72@yandex.ru',
+    subject: 'Заявка',
+    html: "Категория: "+req.body.a+" "+req.body.b+" "+req.body.c+" "+req.body.d+" "+req.body.e+"<br /><br />"+
+    "Район: "+req.body.center+" "+req.body.viz+" "+req.body.uralmash+" "+req.body.sort+" "+req.body.kirov+" "+req.body.chkalov+"<br /><br />"+
+    "Тип обучения: "+req.body.online+" "+req.body.group+"<br /><br />"+
+    "Тип трансмиссии: "+req.body.mkpp+" "+req.body.akpp+"<br /><br />"+
+    "Номер телефона: "+req.body.phone
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+    if(!error)
+        res.render('thank', { title: 'Спасибо за заявку'})
+    else
+      res.send("Mail not sent")
+  });
+});
+
+router.get('/pdd', function(req, res){
+  res.render('pdd/index', { title: 'Экзамен ПДД Онлайн' });
+});
+
+router.get('/thank', function(req, res){
+  res.render('thank', { title: 'Спасибо за заявку'})
+})
+
+router.get('/contact', function(req, res){
+  res.render('contact', { title: 'Форма заявки' });
+});
+
+router.get('/personal', function(req, res){
+  res.render('personal', { title: 'Согласие на обработку персональных данных' });
+});
+module.exports = router;
---
+++ tobolsk-school.ru/routes/schools.js
@@ -0,0 +1,100 @@
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose'), //mongo connection
+    bodyParser = require('body-parser'), //parses information from POST
+    methodOverride = require('method-override'); //used to manipulate POST
+
+//Any requests to this controller must pass through this 'use' function
+//Copy and pasted from method-override
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+}))
+
+//build the REST operations at the base for blobs
+//this will be accessible from http://127.0.0.1:3000/blobs if the default route for / is left unchanged
+router.route('/')
+    //GET all blobs
+    .get(function(req, res, next) {
+        //retrieve all blobs from Monogo
+        mongoose.model('School').find({}, function (err, schools) {
+              if (err) {
+                  return console.error(err);
+              } else {
+                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                  res.format({
+                      //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
+                    html: function(){
+                        res.render('schools/index', {
+                              title: 'Список Автошкол',
+                              "schools" : schools
+                          });
+                    },
+                    //JSON response will show all blobs in JSON format
+                    json: function(){
+                        res.json(schools);
+                    }
+                });
+              }
+        });
+    })
+
+// route middleware to validate :id
+router.param('id', function(req, res, next, id) {
+    //console.log('validating ' + id + ' exists');
+    //find the ID in the Database
+    mongoose.model('School').findById(id, function (err, school) {
+        //if it isn't found, we are going to repond with 404
+        if (err) {
+            console.log(id + ' was not found');
+            res.status(404)
+            var err = new Error('Not Found');
+            err.status = 404;
+            res.format({
+                html: function(){
+                    next(err);
+                 },
+                json: function(){
+                       res.json({message : err.status  + ' ' + err});
+                 }
+            });
+        //if it is found we continue on
+        } else {
+            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
+            //console.log(blob);
+            // once validation is done save the new item in the req
+            req.id = id;
+            // go to the next thing
+            next();
+        }
+    });
+});
+
+router.route('/:id')
+  .get(function(req, res) {
+    mongoose.model('School').findById(req.id, function (err, school) {
+      if (err) {
+        console.log('GET Error: There was a problem retrieving: ' + err);
+      } else {
+        console.log('GET Retrieving ID: ' + school._id);
+        res.format({
+          html: function(){
+              res.render('schools/show', {
+                title: 'Автошкола ' + school.name,
+                "school" : school
+              });
+          },
+          json: function(){
+              res.json(school);
+          }
+        });
+      }
+    });
+  });
+
+module.exports = router;
